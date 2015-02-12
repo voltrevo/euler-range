151c888e03b6786126de9a1e15758c06adf44ce7
Andrew Morris <voltrevo@gmail.com>
2015-02-13 08:43:45 +1100

    Added some docs.

--- projEuler.js
+++ projEuler.js
@@ -90,6 +90,7 @@ var solutions = [
         return range.primes().head(10001).last()
     },
     function p8() {
+        // Find the 13 consecutive digits below with the largest product.
         var digits = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
 
         return range
@@ -117,12 +118,14 @@ var solutions = [
             .first()
     },
     function p10() {
+        // Find the sum of all primes below 2,000,000.
         return range
             .primes()
             .while(function(p) { return p < 2000000 })
             .sum()
     },
     function p11() {
+        // Find the largest product of four digits below that form a row, column, or diagonal.
         var grid = [
             [ 8,  2, 22, 97, 38, 15,  0, 40,  0, 75,  4,  5,  7, 78, 52, 12, 50, 77, 91,  8],
             [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48,  4, 56, 62,  0],
@@ -218,11 +221,12 @@ var solutions = [
 
                 return [n, count]
             })
-            .filter(function(x) { return x[1] > 500 })
-            .map(function(x) { return x[0] })
+            .filter(args(function(n, count) { return count > 500 }))
+            .map(args(function(n/*, count*/) { return n }))
             .first()
     },
     function p13() {
+        // Find the first 10 digits of the sum of the following numbers.
         var strNums = [
             "37107287533902102798797998220837590246510135740250",
             "46376937677490009712648124896970078050417018260538",
@@ -335,6 +339,7 @@ var solutions = [
             .substring(0, 10)
     },
     function p14() {
+        // Find the longest collatz sequence starting less than 1,000,000.
         return 'skipped'
         return range
             .interval(1, 1000000)
@@ -357,6 +362,8 @@ var solutions = [
             })[0]
     },
     function p15() {
+        // Find the number of paths from the top-left to the bottom-right through a 20x20 grid only moving right or down.
+
         // Always takes 40 movements, 20 down and 20 right, but jumbled.
         // This amounts to choosing 20 places to put down movements among 40 slots, which is 40 C 20.
 
@@ -367,6 +374,7 @@ var solutions = [
         return ncr(40, 20)
     },
     function p16() {
+        // Find the sum of the digits of 2^1000.
         return range
             .fromString(
                 bigint(2)
@@ -376,6 +384,7 @@ var solutions = [
             .sum()
     },
     function p17() {
+        // Find the number of letters in "one, two, three, ... one thousand" without commas or spaces.
         function numWords(n) {
             var digits = [
                 "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
@@ -415,6 +424,7 @@ var solutions = [
             .sum()
     },
     function p18() {
+        // Find the path from top to bottom in the pyramid below with the largest sum.
         var pyramid = [
                                         [75],
                                       [95, 64],
@@ -451,6 +461,7 @@ var solutions = [
             .max()
     },
     function p19() {
+        // How many Sundays fell on the first day of the month during the 20th century?
         return range
             .cross(
                 range.interval(1901, 2001),
@@ -461,6 +472,7 @@ var solutions = [
             .length()
     },
     function p20() {
+        // Find the sum of the digits in 100!
         return range
             .fromString(
                 range
@@ -476,6 +488,7 @@ var solutions = [
 range
     .fromArray(solutions)
     .map(function(f) {
+        // Time each solution
         var start = new Date()
         var value = f()
         var end = new Date()
