d86a210a29b96878990702b6f307236e92ea2d81
Andrew Morris <voltrevo@gmail.com>
2015-03-10 19:56:52 +1100

    Browserify compatibility

--- projEuler/run.js
+++ projEuler/run.js
@@ -4,19 +4,36 @@ var range = require("../index")
 
 range
     .interval(1, 21)
-    .map(function(n) { return require("./problems/p" + n + ".js") })
-    .map(function(f) {
+    .filter(function(i) {
+        return (
+            !process.browser ||
+            (
+                i !== 13 &&
+                i !== 16 &&
+                i !== 20
+            )
+        )
+    })
+    .map(function(i) {
+        return {
+            index: i,
+            fn: require(
+                (process.browser ? "/projEuler/problems/" : "") +
+                "p" + i + ".js")
+        }
+    })
+    .map(function(x) {
         // Time each solution
         var start = new Date()
-        var value = f()
+        var value = x.fn()
         var end = new Date()
 
         return {
             value: value,
-            time: end - start
+            time: end - start,
+            index: x.index
         }
     })
-    .combine('index', range.numbers(1))
     .map(function(problem) {
         return problem.index + ": " + problem.value + " (" + problem.time + "ms)"
     })
